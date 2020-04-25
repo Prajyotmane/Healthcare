5e5d13834bac1a5d7a4a2f9d782ca2ed754b781d
Prajyot <prajyotmane464@gmail.com>
2020-04-25 01:26:06 -0700

    Login and register commit

--- app/src/main/java/com/prajyotmane/healthcare/LoginActivity.kt
+++ app/src/main/java/com/prajyotmane/healthcare/LoginActivity.kt
@@ -3,17 +3,49 @@ package com.prajyotmane.healthcare
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class LoginActivity : AppCompatActivity() {
+    var mAuth:FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        mAuth = FirebaseAuth.getInstance()
     }
     fun loginUser(view:View){
+        var email = emailId.text.trim().toString()
+        var password = passwd.text.trim().toString()
 
-    }
+        if (email.isEmpty()) {
+            emailId.setError("Email cannot be empty")
+            return
+        }
+        if (password.isEmpty()) {
+            passwd.setError("Password cannot be empty")
+            return
+        }
+
+
+        mAuth?.signInWithEmailAndPassword(email, password)
+            ?.addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Toast.makeText(this,"Login successful", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this,HomePage::class.java))
+
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(this,"Invalid Email ID or Password", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+}
     fun registerUser(view:View){
         startActivity(Intent(this,RegisterActivity::class.java))
     }
